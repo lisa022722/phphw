1b5976b7a85695bf0df722b9a6d645ac9278cb8f
lisa022722 <lisa022722@gmail.com>
2017-05-03 01:34:56 +0800

    Add files via upload

--- week10hw.php
+++ week10hw.php
@@ -1,47 +1,47 @@
+<?php
 
 
-		<form action=""  enctype="multipart/form-data"  method="post">
-
-
+	echo '<meta charset="utf-8">';
+	$uploadnum=$_GET["uploadnum"];
 		
 
-		<?php
-
-		$uploadnum=$_POST["uploadnum"];
+		echo '<form action=""  enctype="multipart/form-data"  method="post">'; //用post才會收到下面檔案
 
 
-			for($a=1;$a<=$uploadnum;$a++){
-		?>	
-				<input type="file" name="file[]" ><br/><br/>
-		<?php
+			for($a=0;$a<$uploadnum;$a++){
+	
+				echo '<input type="file" name="file[]" ><br/><br/>';
+				
 			}
-		?>
+		
 			
 		
-		<input type="submit" name="file" value="上傳檔案">
+		echo '<input type="submit" name="file" value="上傳檔案">';
+
+		echo '</form>';
+
+		
+		if(isset($_FILES["file"])){
+
+			for($a=0;$a<$uploadnum;$a++){
+				echo "檔案名稱:".$_FILES["file"]["name"][$a]."<br/>";
+				echo "暫存檔名:".$_FILES["file"]["tmp_name"][$a]."<br/>";
+				echo "檔案尺寸:".$_FILES["file"]["size"][$a]."<br/>";
+				echo "檔案種類:".$_FILES["file"]["type"][$a]."<br/>";
+
+				$name=$_FILES["file"]["name"][$a];
+				$tmp=$_FILES["file"]["tmp_name"][$a];
+				if(!empty($name)){
+					copy($tmp,$name);
+					echo "檔案上傳成功<br/><br/>";
+					unlink($tmp);
+				}else{
+					echo "檔案上傳失敗";
+					}
+
+			}
+		}		
+	
 
 
-		<?php
-			if(isset($_FILES["file"])){
-			echo "檔案名稱:".$_FILES["file"]["name"][$a]."<br/>";
-			echo "暫存檔名:".$_FILES["file"]["tmp_name"][$a]."<br/>";
-			echo "檔案尺寸:".$_FILES["file"]["size"][$a]."<br/>";
-			echo "檔案種類:".$_FILES["file"]["type"][$a]."<br/>";
-
-			$name=$_FILES["file"]["name"][$a];
-			$tmp=$_FILES["file"]["tmp_name"][$a];
-			if(!empty($name)){
-				copy($tmp,$name);
-				echo "檔案上傳成功<br>";
-			}else{
-				echo "檔案上傳失敗";
-				}
-
-			}		
-		?>
-
-</form>
-
-
-
-
+?>
